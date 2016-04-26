9a03a5445ab1bfc874af5be1188a0e794415038e
Gralex <aleksandr.grigorchuk@gmail.com>
2016-04-26 18:24:19 +0300

    Fix language files

--- mod_jmodule.php
+++ mod_jmodule.php
@@ -11,6 +11,18 @@ defined('_JEXEC') or die;
 // Включть воспомогательные функции
 require_once __DIR__ . '/helper.php';
 
+// если в параметрах модуля установлено обрабатывать плагинами разрешаем обработку
+if ($params->def('prepare_content', 1))
+{
+    JPluginHelper::importPlugin('content');
+    $module->content = JHtml::_('content.prepare', $module->content, '', 'mod_jmodule.content');
+}
+
+// получаем остальные параметры плагина
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+$field1 = htmlspecialchars($params->get('field1', 'jmodule'));
+
+// получаем контент
 $list = ModJModuleHelper::getList($params);
 
 require JModuleHelper::getLayoutPath('mod_jmodule', $params->get('layout', 'default'));
